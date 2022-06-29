72ce555ed8dc5fc1714de2dd9b5e33b2ea27a604
Kimin Ryu <ryu@holix.com>
2022-06-30 03:19:34 +0900

    apply publish-root.gradle + buildSrc

---
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Versions.kt
@@ -0,0 +1,34 @@
+package com.holix.android.bottomsheetdialog.compose
+
+object Versions {
+    internal const val ANDROID_GRADLE_PLUGIN = "7.2.0"
+    internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
+    internal const val KOTLIN = "1.6.21"
+    internal const val KOTLIN_COROUTINE = "1.6.1"
+
+    internal const val MATERIAL = "1.6.1"
+    internal const val ANDROIDX_CORE_KTX = "1.8.0"
+
+    internal const val COMPOSE = "1.2.0-rc02"
+    internal const val COMPOSE_ACTIVITY = "1.4.0"
+}
+
+object Dependencies {
+    const val androidGradlePlugin =
+        "com.android.tools.build:gradle:${Versions.ANDROID_GRADLE_PLUGIN}"
+    const val gradleNexusPublishPlugin =
+        "io.github.gradle-nexus:publish-plugin:${Versions.GRADLE_NEXUS_PUBLISH_PLUGIN}"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+    const val coroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.KOTLIN_COROUTINE}"
+
+    const val material = "com.google.android.material:material:${Versions.MATERIAL}"
+    const val androidxCoreKtx = "androidx.core:core-ktx:${Versions.ANDROIDX_CORE_KTX}"
+
+    const val composeUI = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+    const val composeRuntime = "androidx.compose.runtime:runtime:${Versions.COMPOSE}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.COMPOSE}"
+    const val composeFoundation = "androidx.compose.foundation:foundation:${Versions.COMPOSE}"
+    const val composeTooling = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+    const val composeActivity = "androidx.activity:activity-compose:${Versions.COMPOSE_ACTIVITY}"
+}
