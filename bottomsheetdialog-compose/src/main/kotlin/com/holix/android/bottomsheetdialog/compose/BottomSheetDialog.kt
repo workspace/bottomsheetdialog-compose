1b5d5b5f1ef48873fd045da70079abf4c0cf3be4
Kimin Ryu <ryu@holix.com>
2022-06-30 00:43:12 +0900

    implement BottomSheetDialog composable and BottomSheetDialogProperties
    - based on Dialog composable
    - replace Android Dialog with BottomSheetDialog from material-components-android
    - set transparent theme for dialog
    - enable set navigationBarColor by property

---
+++ bottomsheetdialog-compose/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/BottomSheetDialog.kt
@@ -0,0 +1,346 @@
+package com.holix.android.bottomsheetdialog.compose
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Outline
+import android.view.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.semantics.dialog
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.SecureFlagPolicy
+import androidx.lifecycle.ViewTreeLifecycleOwner
+import androidx.lifecycle.ViewTreeViewModelStoreOwner
+import androidx.savedstate.findViewTreeSavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
+
+
+/**
+ * Properties used to customize the behavior of a [Dialog].
+ *
+ * @property dismissOnBackPress whether the dialog can be dismissed by pressing the back button.
+ * If true, pressing the back button will call onDismissRequest.
+ * @property dismissOnClickOutside whether the dialog can be dismissed by clicking outside the
+ * dialog's bounds. If true, clicking outside the dialog will call onDismissRequest.
+ * @property securePolicy Policy for setting [WindowManager.LayoutParams.FLAG_SECURE] on the
+ * dialog's window.
+ * @property usePlatformDefaultWidth Whether the width of the dialog's content should be limited to
+ * the platform default, which is smaller than the screen width.
+ */
+@Immutable
+class BottomSheetDialogProperties constructor(
+    val dismissOnBackPress: Boolean = true,
+    val dismissOnClickOutside: Boolean = true,
+    val securePolicy: SecureFlagPolicy = SecureFlagPolicy.Inherit,
+    val navigationBarColor: Color = Color.Unspecified
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is DialogProperties) return false
+
+        if (dismissOnBackPress != other.dismissOnBackPress) return false
+        if (dismissOnClickOutside != other.dismissOnClickOutside) return false
+        if (securePolicy != other.securePolicy) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = dismissOnBackPress.hashCode()
+        result = 31 * result + dismissOnClickOutside.hashCode()
+        result = 31 * result + securePolicy.hashCode()
+        return result
+    }
+}
+
+/**
+ * Opens a dialog with the given content.
+ *
+ * The dialog is visible as long as it is part of the composition hierarchy.
+ * In order to let the user dismiss the Dialog, the implementation of [onDismissRequest] should
+ * contain a way to remove to remove the dialog from the composition hierarchy.
+ *
+ * Example usage:
+ *
+ * @sample androidx.compose.ui.samples.DialogSample
+ *
+ * @param onDismissRequest Executes when the user tries to dismiss the dialog.
+ * @param properties [DialogProperties] for further customization of this dialog's behavior.
+ * @param content The content to be displayed inside the dialog.
+ */
+@Composable
+fun BottomSheetDialog(
+    onDismissRequest: () -> Unit,
+    properties: BottomSheetDialogProperties = BottomSheetDialogProperties(),
+    content: @Composable () -> Unit
+) {
+    val view = LocalView.current
+    val density = LocalDensity.current
+    val layoutDirection = LocalLayoutDirection.current
+    val composition = rememberCompositionContext()
+    val currentContent by rememberUpdatedState(content)
+    val dialogId = rememberSaveable { UUID.randomUUID() }
+    val dialog = remember(view, density) {
+        BottomSheetDialogWrapper(
+            onDismissRequest,
+            properties,
+            view,
+            layoutDirection,
+            density,
+            dialogId
+        ).apply {
+            setContent(composition) {
+                // TODO(b/159900354): draw a scrim and add margins around the Compose Dialog, and
+                //  consume clicks so they can't pass through to the underlying UI
+                BottomSheetDialogLayout(
+                    Modifier.semantics { dialog() },
+                ) {
+                    currentContent()
+                }
+            }
+        }
+    }
+
+    DisposableEffect(dialog) {
+        dialog.show()
+
+        onDispose {
+            dialog.dismiss()
+            dialog.disposeComposition()
+        }
+    }
+
+    SideEffect {
+        dialog.updateParameters(
+            onDismissRequest = onDismissRequest,
+            properties = properties,
+            layoutDirection = layoutDirection
+        )
+    }
+}
+
+/**
+ * Provides the underlying window of a dialog.
+ *
+ * Implemented by dialog's root layout.
+ */
+interface DialogWindowProvider {
+    val window: Window
+}
+
+@Suppress("ViewConstructor")
+private class BottomSheetDialogLayout(
+    context: Context,
+    override val window: Window
+) : AbstractComposeView(context), DialogWindowProvider {
+    private var content: @Composable () -> Unit by mutableStateOf({})
+
+    override var shouldCreateCompositionOnAttachedToWindow: Boolean = false
+        private set
+
+    fun setContent(parent: CompositionContext, content: @Composable () -> Unit) {
+        setParentCompositionContext(parent)
+        this.content = content
+        shouldCreateCompositionOnAttachedToWindow = true
+        createComposition()
+    }
+
+    @Composable
+    override fun Content() {
+        content()
+    }
+}
+
+private class BottomSheetDialogWrapper(
+    private var onDismissRequest: () -> Unit,
+    private var properties: BottomSheetDialogProperties,
+    private val composeView: View,
+    layoutDirection: LayoutDirection,
+    density: Density,
+    dialogId: UUID
+) : BottomSheetDialog(
+    /**
+     * [Window.setClipToOutline] is only available from 22+, but the style attribute exists on 21.
+     * So use a wrapped context that sets this attribute for compatibility back to 21.
+     */
+    ContextThemeWrapper(composeView.context, R.style.TransparentBottomSheetTheme)
+),
+    ViewRootForInspector {
+    private val bottomSheetDialogLayout: BottomSheetDialogLayout
+
+    private val maxSupportedElevation = 30.dp
+
+    override val subCompositionView: AbstractComposeView get() = bottomSheetDialogLayout
+
+    init {
+        val window = window ?: error("Dialog has no window")
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+        bottomSheetDialogLayout = BottomSheetDialogLayout(context, window).apply {
+            // Set unique id for AbstractComposeView. This allows state restoration for the state
+            // defined inside the Dialog via rememberSaveable()
+            setTag(androidx.compose.ui.R.id.compose_view_saveable_id_tag, "Dialog:$dialogId")
+            // Enable children to draw their shadow by not clipping them
+            clipChildren = false
+            // Allocate space for elevation
+            with(density) { elevation = maxSupportedElevation.toPx() }
+            // Simple outline to force window manager to allocate space for shadow.
+            // Note that the outline affects clickable area for the dismiss listener. In case of
+            // shapes like circle the area for dismiss might be to small (rectangular outline
+            // consuming clicks outside of the circle).
+            outlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View, result: Outline) {
+                    result.setRect(0, 0, view.width, view.height)
+                    // We set alpha to 0 to hide the view's shadow and let the composable to draw
+                    // its own shadow. This still enables us to get the extra space needed in the
+                    // surface.
+                    result.alpha = 0f
+                }
+            }
+        }
+        this.behavior.addBottomSheetCallback(
+            object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    if (newState == STATE_HIDDEN) {
+                        onDismissRequest()
+                    }
+                }
+            }
+        )
+
+        /**
+         * Disables clipping for [this] and all its descendant [ViewGroup]s until we reach a
+         * [BottomSheetDialogLayout] (the [ViewGroup] containing the Compose hierarchy).
+         */
+        fun ViewGroup.disableClipping() {
+            clipChildren = false
+            if (this is BottomSheetDialogLayout) return
+            for (i in 0 until childCount) {
+                (getChildAt(i) as? ViewGroup)?.disableClipping()
+            }
+        }
+
+        // Turn of all clipping so shadows can be drawn outside the window
+        (window.decorView as? ViewGroup)?.disableClipping()
+        setContentView(bottomSheetDialogLayout)
+        ViewTreeLifecycleOwner.set(bottomSheetDialogLayout, ViewTreeLifecycleOwner.get(composeView))
+        ViewTreeViewModelStoreOwner.set(bottomSheetDialogLayout, ViewTreeViewModelStoreOwner.get(composeView))
+        bottomSheetDialogLayout.setViewTreeSavedStateRegistryOwner(
+            composeView.findViewTreeSavedStateRegistryOwner()
+        )
+
+        // Initial setup
+        updateParameters(onDismissRequest, properties, layoutDirection)
+    }
+
+    private fun setLayoutDirection(layoutDirection: LayoutDirection) {
+        bottomSheetDialogLayout.layoutDirection = when (layoutDirection) {
+            LayoutDirection.Ltr -> android.util.LayoutDirection.LTR
+            LayoutDirection.Rtl -> android.util.LayoutDirection.RTL
+        }
+    }
+
+    // TODO(b/159900354): Make the Android Dialog full screen and the scrim fully transparent
+
+    fun setContent(parentComposition: CompositionContext, children: @Composable () -> Unit) {
+        bottomSheetDialogLayout.setContent(parentComposition, children)
+    }
+
+    private fun setSecurePolicy(securePolicy: SecureFlagPolicy) {
+        val secureFlagEnabled =
+            securePolicy.shouldApplySecureFlag(composeView.isFlagSecureEnabled())
+        window!!.setFlags(
+            if (secureFlagEnabled) {
+                WindowManager.LayoutParams.FLAG_SECURE
+            } else {
+                WindowManager.LayoutParams.FLAG_SECURE.inv()
+            },
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+
+    private fun setNavigationBarColor(color: Color) {
+        if (color != Color.Unspecified) {
+            window!!.navigationBarColor = color.toArgb()
+        }
+    }
+
+    fun updateParameters(
+        onDismissRequest: () -> Unit,
+        properties: BottomSheetDialogProperties,
+        layoutDirection: LayoutDirection
+    ) {
+        this.onDismissRequest = onDismissRequest
+        this.properties = properties
+        setSecurePolicy(properties.securePolicy)
+        setLayoutDirection(layoutDirection)
+        setCanceledOnTouchOutside(properties.dismissOnClickOutside)
+        setNavigationBarColor(properties.navigationBarColor)
+    }
+
+    fun disposeComposition() {
+        bottomSheetDialogLayout.disposeComposition()
+    }
+
+    override fun cancel() {
+        onDismissRequest()
+    }
+
+    override fun onBackPressed() {
+        if (properties.dismissOnBackPress) {
+            onDismissRequest()
+        }
+    }
+}
+
+@Composable
+private fun BottomSheetDialogLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        val placeables = measurables.map { it.measure(constraints) }
+        val width = placeables.maxByOrNull { it.width }?.width ?: constraints.minWidth
+        val height = placeables.maxByOrNull { it.height }?.height ?: constraints.minHeight
+        layout(width, height) {
+            placeables.forEach { it.placeRelative(0, 0) }
+        }
+    }
+}
+
+fun View.isFlagSecureEnabled(): Boolean {
+    val windowParams = rootView.layoutParams as? WindowManager.LayoutParams
+    if (windowParams != null) {
+        return (windowParams.flags and WindowManager.LayoutParams.FLAG_SECURE) != 0
+    }
+    return false
+}
+
+fun SecureFlagPolicy.shouldApplySecureFlag(isSecureFlagSetOnParent: Boolean): Boolean {
+    return when (this) {
+        SecureFlagPolicy.SecureOff -> false
+        SecureFlagPolicy.SecureOn -> true
+        SecureFlagPolicy.Inherit -> isSecureFlagSetOnParent
+    }
+}
