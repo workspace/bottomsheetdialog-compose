0d12ef8f6f34bccb58b44e8a1ddcffc4a81317c8
Kimin Ryu <ryu@holix.com>
2022-10-27 14:59:09 +0900

    improve sample
    able to set BottomSheetBehaviorProperties in sample app

--- app/src/main/kotlin/com/holix/android/bottomsheetdialogcomposedemo/preferences/ColorPreference.kt
+++ app/src/main/kotlin/com/holix/android/bottomsheetdialogcomposedemo/preferences/ColorPreference.kt
@@ -1,6 +1,7 @@
 package com.holix.android.bottomsheetdialogcomposedemo.preferences
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -8,7 +9,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,8 +16,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import com.github.skydoves.colorpicker.compose.*
+import com.holix.android.bottomsheetdialog.compose.BottomSheetBehaviorProperties
+import com.holix.android.bottomsheetdialog.compose.BottomSheetDialog
+import com.holix.android.bottomsheetdialog.compose.BottomSheetDialogProperties
 
 @Composable
 fun ColorPreference(
@@ -27,15 +29,21 @@ fun ColorPreference(
 ) {
     val surfaceColor = MaterialTheme.colors.surface
     val currentOnValueChange by rememberUpdatedState(onValueChange)
-    var showColorPickerDialog by rememberSaveable {
+    var showColorPickerDialog by remember {
         mutableStateOf(false)
     }
     if (showColorPickerDialog) {
         var targetColor by remember {
             mutableStateOf(value)
         }
-        Dialog(
-            onDismissRequest = { showColorPickerDialog = false }
+        BottomSheetDialog(
+            onDismissRequest = { showColorPickerDialog = false },
+            properties = BottomSheetDialogProperties(
+                behaviorProperties = BottomSheetBehaviorProperties(
+                    state = BottomSheetBehaviorProperties.State.Expanded,
+                    skipCollapsed = true
+                )
+            )
         ) {
             Surface {
                 Column(
@@ -91,7 +99,8 @@ fun ColorPreference(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp, 8.dp),
+            .clickable { showColorPickerDialog = true }
+            .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
@@ -99,12 +108,6 @@ fun ColorPreference(
         when (value) {
             MaterialTheme.colors.surface -> {
                 Text("Default - MaterialTheme.colors.surface")
-                Spacer(modifier = Modifier.weight(1F))
-                Button(
-                    onClick = { showColorPickerDialog = true }
-                ) {
-                    Text("Set")
-                }
             }
             else -> {
                 Box(
--- app/src/main/kotlin/com/holix/android/bottomsheetdialogcomposedemo/preferences/SingleChoicePreference.kt
+++ app/src/main/kotlin/com/holix/android/bottomsheetdialogcomposedemo/preferences/SingleChoicePreference.kt
@@ -1,19 +1,18 @@
 package com.holix.android.bottomsheetdialogcomposedemo.preferences
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
 @Composable
 fun SingleChoicePreference(
@@ -23,29 +22,62 @@ fun SingleChoicePreference(
     options: List<Pair<String, String>>,
 ) {
     val currentOnValueChange by rememberUpdatedState(onValueChange)
-    Column(
+    var showChoiceDialog by remember {
+        mutableStateOf(false)
+    }
+    if (showChoiceDialog) {
+        Dialog(
+            onDismissRequest = { showChoiceDialog = false }
+        ) {
+            Surface(
+                shape = RoundedCornerShape(16.dp)
+            ) {
+                Column {
+                    options.forEach { (option, description) ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    currentOnValueChange(option)
+                                    showChoiceDialog = false
+                                }
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = description,
+                                color = if (option == value) {
+                                    MaterialTheme.colors.primary
+                                } else {
+                                    Color.Unspecified
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp, 8.dp)
+            .clickable { showChoiceDialog = true }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = label,
             style = MaterialTheme.typography.body1,
             fontWeight = FontWeight.Bold
         )
-        options.forEach { (option, description) ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                RadioButton(
-                    selected = option == value,
-                    onClick = { currentOnValueChange(option) })
+        options
+            .firstOrNull { (option, _) -> option == value }
+            ?.let { (_, description) ->
+                Spacer(modifier = Modifier.width(16.dp))
                 Text(
-                    text = description,
-                    style = MaterialTheme.typography.body2
+                    modifier = Modifier.weight(1F),
+                    text = description
                 )
             }
-        }
     }
 }
