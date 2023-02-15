1e2ef5facae4a37b69fd6b31801f272d6cb5d886
Kimin Ryu <ryu@holix.com>
2023-02-15 11:54:26 +0900

    Compose BOM 2023.01.00

--- buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Versions.kt
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
     internal const val MATERIAL = "1.8.0"
     internal const val ANDROIDX_CORE_KTX = "1.9.0"
 
-    internal const val COMPOSE = "1.3.0"
+    internal const val COMPOSE_BOM = "2023.01.00"
     const val COMPOSE_COMPILER = "1.3.2"
     internal const val ANDROIDX_ACTIVITY = "1.5.1"
 
@@ -26,12 +26,13 @@ object Dependencies {
     const val androidxActivityKtx = "androidx.activity:activity-ktx:${Versions.ANDROIDX_ACTIVITY}"
     const val androidxCoreKtx = "androidx.core:core-ktx:${Versions.ANDROIDX_CORE_KTX}"
 
-    const val composeUI = "androidx.compose.ui:ui:${Versions.COMPOSE}"
-    const val composeRuntime = "androidx.compose.runtime:runtime:${Versions.COMPOSE}"
-    const val composeMaterial = "androidx.compose.material:material:${Versions.COMPOSE}"
-    const val composeFoundation = "androidx.compose.foundation:foundation:${Versions.COMPOSE}"
-    const val composeTooling = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
-    const val composeActivity = "androidx.activity:activity-compose:${Versions.COMPOSE_ACTIVITY}"
+    const val composeBom = "androidx.compose:compose-bom:${Versions.COMPOSE_BOM}"
+    const val composeUI = "androidx.compose.ui:ui"
+    const val composeRuntime = "androidx.compose.runtime:runtime"
+    const val composeMaterial = "androidx.compose.material:material"
+    const val composeFoundation = "androidx.compose.foundation:foundation"
+    const val composeTooling = "androidx.compose.ui:ui-tooling"
+    const val composeActivity = "androidx.activity:activity-compose:${Versions.ANDROIDX_ACTIVITY}"
 
     const val colorPicker = "com.github.skydoves:colorpicker-compose:${Versions.COLOR_PICKER}"
 }
