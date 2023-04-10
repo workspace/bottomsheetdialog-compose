5fedf0558f71a6cd642650030181e7cf45b64070
Kimin Ryu <ryu@holix.com>
2023-04-11 00:32:49 +0900

    Release 1.3.0

--- buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
@@ -5,10 +5,10 @@ object Configuration {
     const val targetSdk = 33
     const val minSdk = 21
     const val majorVersion = 1
-    const val minorVersion = 2
-    const val patchVersion = 1
+    const val minorVersion = 3
+    const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 121
+    const val versionCode = 130
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "com.holix.android"
 }
