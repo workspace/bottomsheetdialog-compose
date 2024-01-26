1a4342336c585b9508b4bcd310ed965d588a2988
workspace <ryukimin.workspace@gmail.com>
2024-01-26 16:50:41 +0900

    Release 1.3.2
    - migrate from groovy to kts
    - bump gradle to 8.5
    - bump kotlin to 1.9.21
    - bump androidx activity to 1.8.2
    - bump androidx core to 1.12.0
    - bump compose compiler to 1.5.7
    - bump material to 1.11.0
    - bump color picker compose to 1.0.6

--- buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
@@ -2,13 +2,12 @@ package com.holix.android.bottomsheetdialog.compose
 
 object Configuration {
     const val compileSdk = 34
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val minSdk = 21
     const val majorVersion = 1
     const val minorVersion = 3
-    const val patchVersion = 1
+    const val patchVersion = 2
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 131
-    const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
+    const val versionCode = 132
     const val artifactGroup = "com.holix.android"
 }
