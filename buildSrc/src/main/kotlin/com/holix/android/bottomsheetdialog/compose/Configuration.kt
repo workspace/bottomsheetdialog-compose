d6fdde1e554a56f85a580cfd2dc51fa9fd4e6ed9
workspace <ryukimin.workspace@gmail.com>
2024-11-16 13:55:13 +0900

    Release 1.4.2
    compose 1.6.0 -> 1.6.8

--- buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
@@ -6,8 +6,8 @@ object Configuration {
     const val minSdk = 21
     const val majorVersion = 1
     const val minorVersion = 4
-    const val patchVersion = 1
+    const val patchVersion = 2
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 141
+    const val versionCode = 142
     const val artifactGroup = "com.holix.android"
 }
