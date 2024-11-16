ac658ca6a1587759a99b6d021344f9995892df5c
workspace <ryukimin.workspace@gmail.com>
2024-11-16 14:09:07 +0900

    Release 1.6.0
    - androidx.activity 1.8.2 -> 1.9.3
    - androidx.core 1.12.0 -> 1.15.0
    - material 1.11.0 -> 1.12.0
    - compose 1.8.0-alpha06

--- buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
+++ buildSrc/src/main/kotlin/com/holix/android/bottomsheetdialog/compose/Configuration.kt
@@ -1,13 +1,13 @@
 package com.holix.android.bottomsheetdialog.compose
 
 object Configuration {
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val targetSdk = 34
     const val minSdk = 21
     const val majorVersion = 1
-    const val minorVersion = 5
-    const val patchVersion = 1
+    const val minorVersion = 6
+    const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 151
+    const val versionCode = 160
     const val artifactGroup = "com.holix.android"
 }
